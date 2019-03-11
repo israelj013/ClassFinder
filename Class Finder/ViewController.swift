64103400d4847af09ad7c73e6221028a914be25d
Du Tran <maclab@ics.uci.edu>
2019-03-11 09:35:54 -0700

    Initial Commit

---
+++ Class Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Class Finder
+//
+//  Created by Kay Lab on 3/11/19.
+//  Copyright © 2019 israelj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
